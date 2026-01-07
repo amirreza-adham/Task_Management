b36692d74910b7cde1358dd939c01640b63098b5
amirr <amirr@ADHAM>
2026-01-07 22:02:41 +0330

    create models

---
+++ Task_Management/Models/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public bool IsDone { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        // Foreign key
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ Task_Management/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoApi.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+
+        // Navigation property
+        public ICollection<TodoItem> TodoItems { get; set; }
+    }
+}
