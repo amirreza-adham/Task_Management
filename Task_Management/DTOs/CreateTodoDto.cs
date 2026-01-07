ee4415d2cfc7da49f90e0158eeecff8a5ff3e226
amirr <amirr@ADHAM>
2026-01-08 00:08:09 +0330

    add todos dtos

---
+++ Task_Management/DTOs/CreateTodoDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TodoApi.DTOs
+{
+    public class CreateTodoDto // در اینجا هم نمیخواهیم کلاینت زمان ساخته شدن تسک، انجام شدن یا نشدن و همچنین آیدی را ببیند 
+    {
+        public string Title { get; set; }
+        public string? Description { get; set; }
+    }
+}
