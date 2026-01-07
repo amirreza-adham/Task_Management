d3f79cc602370c1760f20d15e1747341e2e1b8a4
amirr <amirr@ADHAM>
2026-01-07 23:03:15 +0330

    create dtos

---
+++ Task_Management/DTOs/LoginUserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TodoApi.DTOs
+{
+    public class LoginUserDto //dto برای لاگین 
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Task_Management/DTOs/RegisterUserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TodoApi.DTOs
+{
+    public class RegisterUserDto //dto برای ثبت نام 
+    { // واسطی بین کلاینت و مدل ها برای اینکه جزئیات مدل ها پنهان بماند 
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
