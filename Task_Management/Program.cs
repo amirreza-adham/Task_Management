a13ebb7f0d3e81587a9275828540dd7145e26ef7
amirr <amirr@ADHAM>
2026-01-07 22:31:16 +0330

    add dbcontext to program

--- Task_Management/Program.cs
+++ Task_Management/Program.cs
@@ -1,4 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// اضافه کردن dbcontext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 
